b5904baf5e88cb807757ea8f0259c4cc68243e3f
Mordred Admin <gogs@fake.local>
2018-06-05 14:24:29 +0200

    Initial commit

---
+++ src/Connection.h
@@ -0,0 +1,90 @@
+#ifndef SAPPHIRE_CONNECTION_H
+#define SAPPHIRE_CONNECTION_H
+
+#include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <mysql.h>
+#include <map>
+
+
+namespace Mysql
+{
+   using optionMap = std::map< enum mysql_option, std::string >;
+   class MySqlBase;
+   class Statement;
+   class PreparedStatement;
+
+   class Connection : public boost::enable_shared_from_this< Connection >
+   {
+   public:
+      Connection( boost::shared_ptr< MySqlBase > pBase,
+                  const std::string& hostName,
+                  const std::string& userName,
+                  const std::string& password,
+                  uint16_t port = 3306);
+
+      Connection( boost::shared_ptr< MySqlBase > pBase,
+                  const std::string& hostName,
+                  const std::string& userName,
+                  const std::string& password,
+                  const optionMap& options,
+                  uint16_t port = 3306 );
+
+      virtual ~Connection();
+
+      void close();
+      bool isClosed() const;
+
+      bool ping();
+      
+      void setOption( enum mysql_option option, const void* arg );
+      void setOption( enum mysql_option option, uint32_t arg );
+      void setOption( enum mysql_option option, const std::string& arg );
+
+      boost::shared_ptr< MySqlBase > getMySqlBase() const;
+
+      void setAutoCommit( bool autoCommit );
+      bool getAutoCommit();
+
+      std::string escapeString( const std::string& inData );
+
+      void setSchema( const std::string& catalog );
+
+      boost::shared_ptr< Statement > createStatement();
+
+      void beginTransaction();
+      void commitTransaction();
+      void rollbackTransaction();
+
+      std::string getSchema();
+
+      std::string getError();
+      uint32_t getErrorNo();
+
+      bool isReadOnly();
+      void setReadOnly( bool readOnly );
+
+      bool isValid();
+
+      bool reconnect();
+
+      boost::shared_ptr< Mysql::PreparedStatement > prepareStatement( const std::string& sql );
+      
+      std::string getLastStatementInfo();
+
+      MYSQL* getRawCon();
+
+   private:
+      boost::shared_ptr< MySqlBase > m_pBase;
+      MYSQL* m_pRawCon;
+      bool m_bConnected;
+
+      Connection( const Connection& );
+      void operator=( Connection& );
+   };
+
+
+}
+
+#endif //SAPPHIRE_CONNECTION_H
---
+++ src/MySqlBase.h
@@ -0,0 +1,38 @@
+#ifndef SAPPHIRE_MYSQLBASE_H
+#define SAPPHIRE_MYSQLBASE_H
+
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <mysql.h>
+#include <string>
+#include <map>
+
+namespace Mysql
+{
+
+using optionMap = std::map< enum mysql_option, std::string >;
+
+class Connection;
+
+class MySqlBase : public boost::enable_shared_from_this< MySqlBase >
+{
+public:
+   MySqlBase();
+
+   ~MySqlBase();
+
+   boost::shared_ptr< Connection > connect( const std::string& hostName, const std::string& userName,
+                                            const std::string& password, uint16_t port );
+   boost::shared_ptr< Connection > connect( const std::string& hostName, const std::string& userName,
+                                            const std::string& password, const optionMap& map, uint16_t port );
+
+   std::string getVersionInfo();
+
+private:
+   MySqlBase( const MySqlBase& );
+   void operator=( MySqlBase& );
+};
+
+}
+
+#endif //SAPPHIRE_MYSQLBASE_H
